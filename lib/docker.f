a9b7962b2bf8889eabc075047e1aa5ba17b9858d
Chris Murray <chris.m.murray@oracle.com>
2022-04-16 04:08:21 -0500

    added lib docker.f

---
+++ lib/docker.f
@@ -0,0 +1,12 @@
+function docker.true {
+    #accepts 0 args.  returns true/false if is a container under docker
+
+    local lcgroup_pid1=`${cmd_cat} /proc/self/cgroup | ${cmd_grep} ^1:`
+
+                                                                                                    #for those running docker daemon
+    [ `${cmd_echo} "${lcgroup_pid1}" | ${cmd_grep} -ci docker` -gt 0 ] && local lexitstring=${true} || local lexitstring=${false}
+                                                                                                    #for those running docker engine
+    [ `${cmd_echo} "${lcgroup_pid1}" | ${cmd_grep} -ci ^1:` -eq 0 ] && local lexitstring=${true} || local lexitstring=${false}
+
+    ${cmd_echo} ${lexitstring}
+}
